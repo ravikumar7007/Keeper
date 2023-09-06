424f0d9724be786381dda7bb6477baed109560a8
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-09-06 14:49:38 +0530

    creating Input Area

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import CreateArea from "./components/CreateArea";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <CreateArea />
       {notes.map((note) => (
         <Note key={note.key} title={note.title} content={note.content} />
       ))}
